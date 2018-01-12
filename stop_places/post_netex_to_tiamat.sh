825a26847ca4a2905b4f8e8f2973ba615efc99b7
Mathieu Hicauber <mhicauber@okina.fr>
2018-01-12 11:56:04 +0100

    upload script updated saving tiamat results into file

--- stop_places/post_netex_to_tiamat.sh
+++ stop_places/post_netex_to_tiamat.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 url_tiamat='https://entur.okina.fr/services/stop_places/netex?';
+#url_tiamat='http://localhost:8585/services/stop_places/netex?';
 url_file_hosted_server='http://naq-dev01.sysnove.net:4567/netex-data';
 
 username="$1";
 password="$2";
 username_file_hosted_server="$3";
 password_file_hosted_server="$4";
+
+mkdir -p netex_output_from_tiamat;
 
 GetToken(){
     RESULT=`curl --data "grant_type=password&client_id=Tiamat&username="${username}"&password="${password} https://auth.okina.fr/auth/realms/rutebanken/protocol/openid-connect/token`;
@@ -24,105 +27,128 @@ CreateTempFolder(){
     cd downloaded-files;
 }
 
-UploadNeptuneFile(){
+UploadGTFSFile(){
     CreateTempFolder;
     GetToken;
-    echo && echo "--------------- Send Neptune File: $1 ---------------" ;
-    echo "--- Download: $1 ---";
-    curl -O -u "${username_file_hosted_server}:${password_file_hosted_server}" "${url_file_hosted_server}/$1";
-    echo "--- Upload Neptune File to Tiamat: $1 ---" ;
-    curl -XPOST -H"Content-Type: application/xml" -H"authorization: bearer $TOKEN" -d@"$1" ${url_tiamat}"$2";
-    echo "--------------- Sent Neptune File successfully: $1 --" && echo;
+    echo && echo "--------------- Send GTFS File: $2 ---------------" ;
+    echo "--- Download: $2 ---";
+    curl -O -u "${username_file_hosted_server}:${password_file_hosted_server}" "${url_file_hosted_server}/$2";
+    echo "--- Upload Netex File to Tiamat: $2 ---" ;
+    curl -XPOST -H"Content-Type: application/xml" -H"authorization: bearer $TOKEN" -d@"$2" ${url_tiamat}"$3" > ../netex_output_from_tiamat/$1.xml
+    echo "--------------- Sent Netex File successfully: $2 --" && echo;
     DeleteTempFolder;
 }
 
 
 ####################################
 # TOPO Places EPCI
-UploadNeptuneFile "naq_topo_epci.xml" "importType=INITIAL&skipOutput=true"
 
-####################################
-# COMMUNAUTE AGGLO
+#UploadGTFSFile "topo" "naq_topo_epci.xml" "importType=INITIAL&skipOutput=false"
+#
+#####################################
+## COMMUNAUTE AGGLO
+#
+### Tulle
+#UploadGTFSFile "TUT" "_TUT_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Bordeaux Métropole
+#UploadGTFSFile "BME_bus" "_BME_shared_data_bus.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#UploadGTFSFile "BME_tram" "_BME_shared_data_tram.xml" "importType=INITIAL&forceStopType=ONSTREET_TRAM&skipOutput=false"
+#
+### Grand Poitiers
+#UploadGTFSFile "VIT" "_VIT_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Perigueux Agglo
+#UploadGTFSFile "PER" "_PER_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Grand Dax/Couralin
+#UploadGTFSFile "COU" "_COU_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Royan
+#UploadGTFSFile "ROY" "_ROY_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### MACS/Yego
+#UploadGTFSFile "YEG" "_YEG_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Brive
+#UploadGTFSFile "BRI" "_BRI_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Limoges
+#UploadGTFSFile "LIM" "_LIM_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### La Rochelle
+#UploadGTFSFile "YEL" "_YEL_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#UploadGTFSFile "RTC" "_RTC_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Rochefort
+#UploadGTFSFile "ROC" "_ROC_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Niort
+#UploadGTFSFile "CAN" "_CAN_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
+#
+### Val de Garonne - Marmande
+#UploadGTFSFile "VDG" "_VDG_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
 
-##Grand Poitiers
-UploadNeptuneFile "_VIT_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:200069854"
 #
-##Perigueux Agglo
-UploadNeptuneFile "_PER_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:200040392"
 #
-##Landes/Couralin
-UploadNeptuneFile "_COU_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:244000675"
 #
-##Landes/Yego
-UploadNeptuneFile "_YEG_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true"
 #
-##Bordeaux Métropole Tram
-UploadNeptuneFile "_CUB_shared_data_tram" "importType=INITIAL&forceStopType=ONSTREET_TRAM&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:243300316"
+######################################
+### FROM SIMs
 ##
-##Bordeaux Métropole Bus
-UploadNeptuneFile "_BME_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:243300316"
+###Angoulème - STGA
+#UploadGTFSFile "ANG" "_ANG_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##TAC Châtellerault
-UploadNeptuneFile "_CHL_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:248600413"
+###TAC Châtellerault
+#UploadGTFSFile "CHL" "_CHL_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##Cognac
-UploadNeptuneFile "_COG_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:200070514"
+###Cognac
+#UploadGTFSFile "COG" "_COG_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##Brive
-UploadNeptuneFile "_BRI_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:200043172"
+###Aéroport La Rochelle
+#UploadGTFSFile "APR" "_APR_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##Niort
-UploadNeptuneFile "_NIO_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:200041317"
-##
-##Tulle
-UploadNeptuneFile "_TUT_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true&targetTopographicPlaces=OKI:TopographicPlace:241927201"
+###BAC Royan
+#UploadGTFSFile "BAC" "_BAC_shared_data.xml" "importType=MERGE&forceStopType=FERRY_STOP&skipOutput=false"
 #
-##La Rochelle
-UploadNeptuneFile "_YEL_shared_data.xml" "importType=INITIAL&forceStopType=ONSTREET_BUS&skipOutput=true"
-UploadNeptuneFile "_RTC_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
+###Aix/Fouras
+#UploadGTFSFile "FAI" "_FAI_shared_data.xml" "importType=INITIAL&forceStopType=FERRY_STOP&skipOutput=false"
 #
 #
-#####################################
+#######################################
+#### DEPARTEMENTS
+
+### Charente-maritime
+#UploadGTFSFile "CHA" "_CHA_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##Aéroport LR
-UploadNeptuneFile "_APR_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
+### Lot-et-Garonne
+#UploadGTFSFile "LGA" "_LGA_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##Aix/Fouras
-UploadNeptuneFile "_FAI_shared_data.xml" "importType=INITIAL&forceStopType=FERRY_STOP&skipOutput=true"
+### Transport 64
+#UploadGTFSFile "PAT" "_PAT_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##BAC Royan
-UploadNeptuneFile "_BAC_shared_data.xml" "importType=MERGE&forceStopType=FERRY_STOP&skipOutput=true"
+### Landes/XLR
+#UploadGTFSFile "LAN" "_LAN_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
+### Transgironde
+#UploadGTFSFile "GIR" "_GIR_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-#####################################
-## DEPARTEMENTS
+### CG23 Creuse
+#UploadGTFSFile "CRE" "_CRE_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
 #
-##Transgironde
-UploadNeptuneFile "_GIR_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
+### Vienne Interurbain
+#UploadGTFSFile "VIE" "_VIE_shared_data_interurbain.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=false"
+
+
+
+######################################
+### SNCF
 #
-##CG23 Creuse
-UploadNeptuneFile "_CRE_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
-#
-##Vienne
-UploadNeptuneFile "_VIE_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
-#
-##Vienne Interurbain
-UploadNeptuneFile "_VIE_shared_data_interurbain.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
-#
-##Landes/XLR
-UploadNeptuneFile "_LAN_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
-#
-##Limousin
-UploadNeptuneFile "_LAN_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
-#
-##Charente-maritime
-UploadNeptuneFile "_CHA_shared_data.xml" "importType=MERGE&forceStopType=ONSTREET_BUS&skipOutput=true"
-#
-#####################################
-#
-##SNCF / Intercités
-UploadNeptuneFile "_SNC_shared_data_intercites.xml" "importType=MERGE&forceStopType=RAIL_STATION&skipOutput=true"
-#
+###SNCF / Intercités
+UploadGTFSFile "SNC_inter" "_SNC_shared_data_intercites.xml" "importType=INITIAL&forceStopType=RAIL_STATION&skipOutput=false"
+
 ##SNCF / TER
-UploadNeptuneFile "_SNC_shared_data_ter.xml" "importType=MERGE&forceStopType=RAIL_STATION&skipOutput=true"
+UploadGTFSFile "SNC_ter" "_SNC_shared_data_ter.xml" "importType=INITIAL&forceStopType=RAIL_STATION&skipOutput=false"
 #
+#
+
